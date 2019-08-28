537be37e5d1c021a0790a44f6bc3338a988460cb
Zachary Hunt <=>
2019-08-28 17:29:48 -0500

    Fixedcompilationerrors

--- scraper.py
+++ scraper.py
@@ -1,13 +1,33 @@
 import urllib.request
 import os
-site = urllib.request.urlopen("http://whatthecommit.com/").read()
-content = str(site)
-start = content.find("<div id=\"content\">") + 23
-end = content.find("\\n</p>\\n<p class=\"permalink\">")
-message = content[start:end]
-# print("Message:", message, "\n")
+from profanity_check import predict
 
-os.system(R"cd C:\Users\zacha\Documents\MEGAsync\Zachary\Self\Programs\Python\CommitMessages")
-os.system("git add scraper.py")
-os.system("git commit -m \"" + str(message) + "\"")
-os.system("git push")
+
+def main():
+    site = urllib.request.urlopen("http://whatthecommit.com/").read()
+    content = str(site)
+    start = content.find("<div id=\"content\">") + 23
+    end = content.find("\\n</p>\\n<p class=\"permalink\">")
+    message = content[start:end]
+    print("Message:", message, "\n")
+
+    message = clean(message)
+    if len(message) == 0:
+        main()
+
+    os.system(R"cd C:\Users\zacha\Documents\MEGAsync\Zachary\Self\Programs\Python\CommitMessages")
+    os.system("git add scraper.py")
+    os.system("git commit -m \"" + str(message) + "\"")
+    os.system("git push")
+
+
+def clean(phrase: str) -> str:
+    cleanphrase = []
+    for word in phrase.split(" "):
+        if predict([word])[0] != 1:
+            cleanphrase.append(word)
+    return "".join(cleanphrase)
+
+
+if __name__ == "__main__":
+    main()
